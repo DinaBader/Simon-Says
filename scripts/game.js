7ef00b1ae7dcaa7240cb1934493279de5a2a19d9
dina <dina19bader@gmail.com>
2023-11-11 14:55:37 +0200

    the buttons start playing

--- scripts/game.js
+++ scripts/game.js
@@ -1,6 +1,6 @@
 let sequence=[]
 let human=[]
-let level=1
+let level=0;
 let welcome_text=document.getElementById("level-title");
 const red=document.getElementById("red");
 const green=document.getElementById("green");
@@ -8,37 +8,45 @@ const blue=document.getElementById("blue");
 const yellow=document.getElementById("yellow");
 
 
-function button_sound(){
-    red.addEventListener("click",()=>{
-        let audio = new Audio();
-        audio.src = "./sounds/red.mp3";
-        audio.play();
-    });
-
-    green.addEventListener("click",()=>{
-        let audio = new Audio();
-        audio.src = "./sounds/green.mp3";
-        audio.play();
-    });
-
-    blue.addEventListener("click",()=>{
-        let audio = new Audio();
-        audio.src = "./sounds/blue.mp3";
-        audio.play();
-    });
+function button_sound(button){
+    if(button=="red"){
+        red.addEventListener("click",()=>{
+            let audio = new Audio();
+            audio.src = "./sounds/red.mp3";
+            audio.play();
+        });
+    }
     
-    yellow.addEventListener("click",()=>{
-        let audio = new Audio();
-        audio.src = "./sounds/yellow.mp3";
-        audio.play();
-    });
+    else if(button=="green"){
+        green.addEventListener("click",()=>{
+            let audio = new Audio();
+            audio.src = "./sounds/green.mp3";
+            audio.play();
+        });
+    }
+    
+    else if(button=="yellow"){
+        yellow.addEventListener("click",()=>{
+            let audio = new Audio();
+            audio.src = "./sounds/yellow.mp3";
+            audio.play();
+        });
+    }
+    
+    else{
+        blue.addEventListener("click",()=>{
+            let audio = new Audio();
+            audio.src = "./sounds/blue.mp3";
+            audio.play();
+        }); 
+    }
+    
 }
 
 document.onclick= function(event) {
     if (event===undefined) event= window.event;
     var target= 'target' in event? event.target : event.srcElement;
-    level_indicator();
-
+    startGame();
 };
 
 function level_indicator(){
@@ -47,12 +55,32 @@ function level_indicator(){
 
 function next_Round(){
     level+=1;
-    const next_Sequence=[...sequence];
-    next_Sequence.push(add_new_tile());
+    level_indicator();
+    const next_sequence=[...sequence];
+    next_sequence.push(add_new_tile());
+    play_button(next_sequence);
 }
 
 function add_new_tile(){
     const tiles=['red','blue','green','yellow'];
     const random = tiles[Math.floor(Math.random() * tiles.length)];
     return random;
+}
+
+function activateTile(last_color){
+    const tile =document.querySelector(`[data-tile='${last_color}']`)
+    tile.classList.add('pressed');
+    button_sound(last_color)
+    setTimeout(()=>{
+        tile.classList.remove('pressed');
+    },300);
+}
+
+function play_button(next_Sequence){
+    const last_color=next_Sequence.length-1;
+    activateTile(next_Sequence[last_color]);
+}
+
+function startGame(){
+    next_Round();
 }
