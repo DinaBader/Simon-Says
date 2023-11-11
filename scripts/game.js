978c8d0d32228e887873c6ad8f3bbdeb124a5023
dina <dina19bader@gmail.com>
2023-11-11 18:19:53 +0200

    cleaned the code and added a function to take user clicks

--- scripts/game.js
+++ scripts/game.js
@@ -1,86 +1,85 @@
-let sequence=[]
-let human=[]
-let level=0;
-let welcome_text=document.getElementById("level-title");
-const red=document.getElementById("red");
-const green=document.getElementById("green");
-const blue=document.getElementById("blue");
-const yellow=document.getElementById("yellow");
+let sequence = [];
+let human = [];
+let level = 0;
+let welcomeText = document.getElementById("level-title");
+const container = document.querySelector(".container");
 
+function buttonSound(button) {
+    let audio = new Audio();
 
-function button_sound(button){
-    if(button=="red"){
-        red.addEventListener("click",()=>{
-            let audio = new Audio();
-            audio.src = "./sounds/red.mp3";
-            audio.play();
-        });
+    if (button == "red") {
+        audio.src = "./sounds/red.mp3";
+    } else if (button == "green") {
+        audio.src = "./sounds/green.mp3";
+    } else if (button == "yellow") {
+        audio.src = "./sounds/yellow.mp3";
+    } else {
+        audio.src = "./sounds/blue.mp3";
     }
-    
-    else if(button=="green"){
-        green.addEventListener("click",()=>{
-            let audio = new Audio();
-            audio.src = "./sounds/green.mp3";
-            audio.play();
-        });
-    }
-    
-    else if(button=="yellow"){
-        yellow.addEventListener("click",()=>{
-            let audio = new Audio();
-            audio.src = "./sounds/yellow.mp3";
-            audio.play();
-        });
-    }
-    
-    else{
-        blue.addEventListener("click",()=>{
-            let audio = new Audio();
-            audio.src = "./sounds/blue.mp3";
-            audio.play();
-        }); 
-    }
-    
+
+    audio.play();
 }
 
-document.onclick= function(event) {
-    if (event===undefined) event= window.event;
-    var target= 'target' in event? event.target : event.srcElement;
+function startGame() {
+    nextRound();
+}
+
+document.addEventListener('keydown',()=>{
     startGame();
-};
+})  
 
-function level_indicator(){
-    welcome_text.innerText="Level "+level;
+function nextRound() {
+    level += 1;
+    levelIndicator();
+    const nextSequence = [...sequence];
+    nextSequence.push(getRandomTile());
+    playButton(nextSequence);
+    sequence = [...nextSequence];
 }
 
-function next_Round(){
-    level+=1;
-    level_indicator();
-    const next_sequence=[...sequence];
-    next_sequence.push(add_new_tile());
-    play_button(next_sequence);
+function levelIndicator() {
+    welcomeText.innerText = "Level " + level;
 }
 
-function add_new_tile(){
-    const tiles=['red','blue','green','yellow'];
+function getRandomTile() {
+    const tiles = ['red', 'blue', 'green', 'yellow'];
     const random = tiles[Math.floor(Math.random() * tiles.length)];
     return random;
 }
 
-function activateTile(last_color){
-    const tile =document.querySelector(`[data-tile='${last_color}']`)
+function playButton(nextSequence) {
+    nextSequence.forEach((color, index) => {
+        setTimeout(() => {
+            activateTile(color);
+        }, (index + 1) * 600);
+    });
+}
+
+function activateTile(lastColor) {
+    const tile = document.querySelector(`[data-tile='${lastColor}']`);
     tile.classList.add('pressed');
-    button_sound(last_color)
-    setTimeout(()=>{
+    buttonSound(lastColor);
+    setTimeout(() => {
         tile.classList.remove('pressed');
-    },300);
+    }, 300);
 }
 
-function play_button(next_Sequence){
-    const last_color=next_Sequence.length-1;
-    activateTile(next_Sequence[last_color]);
+container.addEventListener('click', event => {
+    const { tile } = event.target.dataset;
+    console.log(tile + " was clicked");
+    if (tile) player_clicks(tile);
+});
+
+
+function player_clicks(tile) {
+    const index=human.push(tile)-1;
+    buttonSound(tile);
+    if(human.length==sequence.length){
+        human=[];
+        setTimeout(()=>{
+            nextRound();
+        },1000);
+    }
+    return;
 }
 
-function startGame(){
-    next_Round();
-}
